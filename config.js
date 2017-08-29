fc563182b219e0a5a7772cd410a47b8a64f192f6
theultimateprasad <prasad.raspaile@gmail.com>
2017-08-29 18:56:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAABnHsXZABIYBAIvAL3R3CNeusyKPWE7sCLaQ8zYRmMSPipE8j7O541sOZAyl3DZBnFZAuTyTc7ljNnofCGv8jZBn1JczP02uJt71nYqZCnZBsOcttbUIZC06TX9Ekr7oT3WnqeSUJlbQlpJbMLoZBw0ZCPtH5Yt48wrbjqFvVG2ZAeeQZDZD',
+    FB_VERIFY_TOKEN: 'prasad1234',
+    API_AI_CLIENT_ACCESS_TOKEN: 'ef4d9a447575468ea549b5deee3fb494',
+    FB_APP_SECRET: '1fa2914126bce12252f855eddbce0064',
+    SERVER_URL: "https://boiling-anchorage-69826.herokuapp.com",
 };
